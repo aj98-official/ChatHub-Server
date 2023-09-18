82d35596214ed4b38cae1f9155c1c00153dea763
Jaiswal <AL55596@ad.wellpoint.com>
2023-09-18 13:49:37 +0530

    add connection for db

---
+++ database/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const user = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+const Connection = async () => {
+
+    const URL = `mongodb+srv://ashutoshjais398:${password}@chathub.fbrdou4.mongodb.net/?retryWrites=true&w=majority`;
+
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewURLParser: true });
+        console.log("Database connected successfully.");
+    }
+    catch (err) {
+        console.log("Error while connecting to database", err.message);
+    }
+}
+
+export default Connection;
