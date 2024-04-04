ae2f848d1018a4a05b84749de88673baf42db47a
Ashutosh <AL55596@ad.wellpoint.com>
2024-04-04 19:33:00 +0530

    format user-controller.js

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,28 +2,30 @@
 import user from "../model/user-model.js";
 
 export const addUser = async (req, res) => {
-    try{
-       let exist = await user.findOne({sub: req.body.sub});
-       if(exist){
-          res.status(200).json({msg: 'user already exists'});
-          return;
-       }
+  try {
+    let exist = await user.findOne({ sub: req.body.sub });
+    if (exist) {
+      res.status(200).json({ msg: 'user already exists' });
+      return;
+    }
 
-       const newUser = new user(req.body);
-       await newUser.save();
-       return res.status(200).json(newUser);
-    }
-    catch(err){
-        return res.status(500).json(err.message);
-    }
-} 
+    const newUser = new user(req.body);
+    await newUser.save();
+    return res.status(200).json(newUser);
+  }
+  catch (err) {
+    return res.status(500).json(err.message);
+  }
+}
 
 export const getUser = async (req, res) => {
-      try{
-        let users = await user.find({});
-        return res.status(200).json(users);
-      }
-      catch(err){
-        return res.status(500).json(err.message);
-      }
+  try {
+    let users = await user.find({});
+    return res.status(200).json(users);
+  }
+  catch (err) {
+    return res.status(500).json(err.message);
+  }
 }
+
+
